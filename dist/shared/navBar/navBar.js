8bd2022ab9967100bb43752a8f25af824520dfc4
paulgoblin <mpr75@cornell.edu>
2016-02-26 15:51:53 -0800

    gh-pages

---
+++ dist/shared/navBar/navBar.js
@@ -0,0 +1,10 @@
+'use strict';
+
+app.directive('navBar', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'shared/navBar/navBar.html',
+    scope: true
+  };
+});
